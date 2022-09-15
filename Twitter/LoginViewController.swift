e364fb3325d514b40fd7d6f11d44ff288ee9f4c5
GabLikesCoffee <gabisgr8@gmail.com>
2022-09-15 14:07:16 -0400

    Added a login button
    Wont work until you either remove the function in the view controller or fill it with something valid

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Gabby Santiago on 9/15/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        TwitterAPICaller.client?.login(url: String, success: <#T##() -> ()#>, failure: <#T##(Error) -> ()#>)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
