c1aa8721d2ffc5f25e36cb15f972909115dc868b
GabLikesCoffee <gabisgr8@gmail.com>
2022-09-15 14:23:38 -0400

    Can log in with home button

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -4,13 +4,14 @@
 //
 //  Created by Dan on 1/3/19.
 //  Copyright © 2019 Dan. All rights reserved.
-//
+// Original consumer key: 5lUJuO5AUpPUCez4ewYDFrtgh
+// Original consumer secret: s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS
 
 import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "5lUJuO5AUpPUCez4ewYDFrtgh", consumerSecret: "s5ynGqXzstUZwFPxVyMDkYh197qvHOcVM3kwv1o2TKhS1avCdS")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "mKvKXDfSB6FE0ysO1CKXlcKI1", consumerSecret: "ZSqKv72mrxpneTnmk4IRp74cOk9A005TIloH3rzKvVUdgb7pMk")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
